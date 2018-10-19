5fd21785479c26fd155d7bea96b54340380f7625
Test Man <ttestman4@gmail.com>
2018-10-19 07:34:39 +0530

    Add navigation buttons for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'lz-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lazyload';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  public gotoMod1() {
+    this.router.navigate(['module1'], { relativeTo: this.route });
+  }
+
+  public gotoMod2() {
+    this.router.navigate(['module2'], { relativeTo: this.route });
+  }
+
+  public gotoMod1Comp1() {
+    this.router.navigate(['/module1/comp1']);
+  }
+
+  public gotoMod1Comp2() {
+    this.router.navigate(['/module1/comp2']);
+  }
+
+  public gotoMod2Comp1() {
+    this.router.navigate(['/module2/comp1']);
+  }
+
+  public gotoMod2Comp2() {
+    this.router.navigate(['/module2/comp2']);
+  }
+
+  public gotoComp1() {
+    this.router.navigate(['../comp1'], { relativeTo: this.route });
+  }
+
+  public gotoComp2() {
+    this.router.navigate(['../comp2'], { relativeTo: this.route });
+  }
 }
--- src/app/module2/mod2comp2/mod2comp2.component.ts
+++ src/app/module2/mod2comp2/mod2comp2.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'lz-mod2comp2',
   templateUrl: './mod2comp2.component.html',
   styles: []
 })
-export class Mod2comp2Component implements OnInit {
+export class Mod2comp2Component {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
 
-  ngOnInit() {
+  public gotoMod1() {
+    this.router.navigate(['/module1']);
+  }
+
+  public gotoMod2() {
+    this.router.navigate(['/module2']);
+  }
+
+  public gotoMod1Comp1() {
+    this.router.navigate(['/module1/comp1']);
+  }
+
+  public gotoMod1Comp2() {
+    this.router.navigate(['/module1/comp2']);
+  }
+
+  public gotoMod2Comp1() {
+    this.router.navigate(['/module2/comp1']);
+  }
+
+  public gotoMod2Comp2() {
+    this.router.navigate(['/module2/comp2']);
+  }
+
+  public gotoComp1() {
+    this.router.navigate(['../comp1'], { relativeTo: this.route });
+  }
+
+  public gotoComp2() {
+    this.router.navigate(['../comp2'], { relativeTo: this.route });
   }
 
 }
